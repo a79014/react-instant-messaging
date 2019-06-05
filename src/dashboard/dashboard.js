9b3c06912b5aaf33abcbf2ae3ba64d311da0d668
César <a79014@alunos.uminho.pt>
2019-06-05 01:12:49 +0100

    Function stubs, plugged them to chatlistcomponent

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,8 +1,39 @@
 import React from "react";
+import ChatListComponent from "../chatlist/chatlist";
 
 class DashboardComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    };
+  }
+
+  newChatBtnClicked = () => {
+    this.setState({ newChatFormVisible: true, selectedChat: null });
+  };
+
+  selectChat = chatIndex => {
+    console.log("selected a chat", chatIndex);
+  };
+
   render() {
-    return <div>Hello world from Dashboard</div>;
+    return (
+      <div>
+        <div>Hello dashboard</div>
+        <ChatListComponent
+          history={this.props.history}
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}
+        />
+      </div>
+    );
   }
 }
 
